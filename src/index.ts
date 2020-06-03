090cf2a0e5f7d2fa8f5ea4a63ebf54dab070ff3d
Corentin Girard <corentingirard.dev@gmail.com>
2020-06-03 22:28:03 +0200

    Added createMatch()

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
-import { Tournament, Teams } from "brackets-model/dist/types"
+import { makePairs } from 'brackets-model';
+import { Tournament, Teams } from 'brackets-model/dist/types'
 
 create({
-    name: "Example",
+    name: 'Example',
     minorOrdering: ['reverse', 'pair_flip', 'reverse', 'natural'],
-    type: "double_elimination",
+    type: 'double_elimination',
     teams: [
-        "Team 1", "Team 2",
-        "Team 3", "Team 4",
-        "Team 5", "Team 6",
-        "Team 7", "Team 8",
-        "Team 9", "Team 10",
-        "Team 11", "Team 12",
-        "Team 13", "Team 14",
-        "Team 15", "Team 16",
+        'Team 1', 'Team 2',
+        'Team 3', 'Team 4',
+        'Team 5', 'Team 6',
+        'Team 7', 'Team 8',
+        'Team 9', 'Team 10',
+        'Team 11', 'Team 12',
+        'Team 13', 'Team 14',
+        'Team 15', 'Team 16',
     ],
 });
 
 function create(tournament: Tournament) {
-    if (tournament.type === "double_elimination") {
+    if (tournament.type === 'double_elimination') {
         createDoubleElimination(tournament.teams);
     }
 }
@@ -27,10 +28,22 @@ function createDoubleElimination(teams: Teams) {
 
     for (let i = roundCount - 1; i >= 0; i--) {
         const matchCount = Math.pow(2, i);
-        createRound(matchCount, teams);
+
+        if (i === roundCount - 1)
+            createRound(matchCount, teams);
+        else
+            createRound(matchCount, []);
     }
 }
 
 function createRound(count: number, teams: Teams) {
-    console.log(count, teams);
+    const allOpponents = makePairs(teams);
+
+    for (let i = 0; i < count; i++) {
+        createMatch(allOpponents[i]);
+    }
+}
+
+function createMatch(opponents?: Teams) {
+    opponents && console.log(opponents);
 }
